14a75054492170a3d132ffba0bd750d8b5c38e28
jasonemarkson <jasonmarkson76@gmail.com>
2021-06-02 22:18:26 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,7 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: {birds: birds, messages: ["hello birds", "goodbye birdies"]}
   end
 end
+# we only use instance variables when rendering erb. When we render JSON we don't need to make it an instance variable
